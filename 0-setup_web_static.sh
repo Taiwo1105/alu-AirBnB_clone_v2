a829d459fb03bcd71855586d35aead18831240dd
Taiwo1105 <t.adegboyeg@alustudent.com>
2024-06-13 10:17:25 -0700

    web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,31 @@
 #!/usr/bin/env bash
-# Static website setup script
+#web_static development
 
-# Update and upgrade the system packages
 sudo apt-get -y update
 sudo apt-get -y upgrade
-
-# Install Nginx if not already installed
 sudo apt-get -y install nginx
-
-# Create the necessary directories for the web content
-sudo mkdir -p /var/www/web_static/releases/demo /var/www/web_static/shared
-
-# Create a sample HTML file
-echo "This is a sample HTML file." | sudo tee /var/www/web_static/releases/demo/sample.html
-
-# Create a custom HTML file for Nginx configuration
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
+# Adding the new configuration for /hbnb_static/0-index.html
 echo "<!DOCTYPE html>
 <html lang=\"en\">
     <head>
         <meta charset=\"UTF-8\" />
-        <title>Demo Page</title>
+        <title>AirBnB clone</title>
     </head>
     <body style=\"margin: 0px; padding: 0px;\">
-        <header style=\"height: 70px; width: 100%; background-color: #0000FF\">
+        <header style=\"height: 65px; width: 100%; background-color: #FF0000\">
         </header>
-        <footer style=\"position: absolute; left: 0; bottom: 0; height: 60px; width: 100%; background-color: #FFFF00; text-align: center; overflow: hidden;\">
-            <p style=\"line-height: 60px; margin: 0px;\">Demo Company</p>
+
+        <footer style=\"position: absolute; left: 0; bottom: 0; height: 50px; width: 100%; background-color: #00FF00; text-align: center; overflow: hidden;\">
+            <p style=\"line-height: 60px; margin: 0px;\">Holberton School</p>
         </footer>
     </body>
-</html>" | sudo tee /var/www/web_static/releases/demo/index.html
+</html>" | sudo tee /data/web_static/releases/test/0-index.html
+# End of new configuration
 
-# Remove any existing symbolic link
-sudo rm -rf /var/www/web_static/current
-
-# Create a new symbolic link to the demo release
-sudo ln -s /var/www/web_static/releases/demo/ /var/www/web_static/current
-
-# Change ownership of the /var/www/ directory
-sudo chown -R webuser:webuser /var/www/
-
-# Update the Nginx configuration to serve content from the static directory
-sudo sed -i '44i \\\n\tlocation /demo_static/ {\n\t\talias /var/www/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-# Restart Nginx to apply the new configuration
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R tetabianca:tetabianca /data/
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
-
